01cb817ac840156bb5c29f4748e8a0da6420fb54
ILay <telatenko92@gmail.com>
2024-07-03 16:22:35 +0400

    Initial commit

---
+++ src/main/java/org/telatenko/photoremodelertelegrambot/PhotoRemodelerTelegramBotApplication.java
@@ -0,0 +1,12 @@
+package org.telatenko.photoremodelertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoRemodelerTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoRemodelerTelegramBotApplication.class, args);
+	}
+}
---
+++ src/test/java/org/telatenko/photoremodelertelegrambot/PhotoRemodelerTelegramBotConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telatenko.photoremodelertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoRemodelerTelegramBotConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
